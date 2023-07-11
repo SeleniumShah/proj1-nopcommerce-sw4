e2b98d175eeb1d00932a1bbd17c989cbb8914cb6
SeleniumShah <shahbhavika475@gmail.com>
2023-07-11 20:43:43 +0100

    homework submitted

---
+++ src/test/java/com/nopcommerce/demo/testsuite/TopMenuTest.java
@@ -0,0 +1,17 @@
+package com.nopcommerce.demo.testsuite;
+
+import com.nopcommerce.demo.pages.TopMenuPage;
+import com.nopcommerce.demo.testbase.BaseTest;
+import org.testng.annotations.Test;
+
+public class TopMenuTest extends BaseTest {
+
+
+    TopMenuPage topMenuPage = new TopMenuPage();
+
+    @Test
+    public void verifyPageNavigation() {
+        topMenuPage.selectMenu("Computers");
+    }
+
+}
